498bda4cf35051e6cc6e8835ba023c1aba479217
Patrick Marohombsar <PMarohombsar89@gmail.com>
2022-04-28 11:28:48 -0400

    Complete

--- index.js
+++ index.js
@@ -209,17 +209,22 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
-
+console.log("Task 1: accessing arrays\n")
+console.log(artists[0]["name"],'\n')
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-
+console.log(artists[2]["bio"],'\n')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 (not auto tested): 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
 
+console.log("Task 2: Fixing a typo\n")
 
+artists[8].name = "Vincent Van Gogh"
+
+console.log(artists[8]["name"], "\n")
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -230,11 +235,15 @@ Use getArtistByIndex to do the following:
 
 🌟 EXAMPLE: if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+console.log("Task 3: Get artist by Index function\n")
+
+function getArtistByIndex(targetArray,targetIndex) {
+  const result = `the artist at index ${targetArray[targetIndex]["id"]} is ${targetArray[targetIndex]["name"]}`
+  console.log(result,"\n")
+  return result
 }
 
-
+getArtistByIndex(artists,9)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use copy to do the following: 
@@ -244,11 +253,16 @@ Use copy to do the following:
 🌟 EXAMPLE of return: ["Amedeo Modigliani", "Vasiliy Kandinskiy", "Diego Rivera"....]
 */
 
-function listOfNames(/*Your Code Here*/) {
-  /*Your Code Here*/
+function listOfNames(targetArray) {
+  const listOfNames = []
+  for (let i = 0; i < (targetArray.length); i++){
+    listOfNames.push(targetArray[i]["name"])
+  }
+  console.log(`Generating a list of names for the requested array...\n`)
+  return listOfNames
 }
 
-
+listOfNames(artists)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use removeArtist to do the following:
@@ -259,10 +273,15 @@ Use removeArtist to do the following:
 5. Return the resulting copied array
 🌟 EXAMPLE: if removeArtist is invoked with the artists array and the number 0, it will return the resulting array with Amedeo Modigliani removed from our dataset. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(targetArray,targetIndex) {
+  const removeArtist = []
+  for (let i = 0; i < targetArray.length; i++) {
+    removeArtist.push(targetArray[i])
+  }
+  removeArtist.splice(targetIndex,1)
+  console.log("Removing element in requested array @ requested index...\n")
+  return removeArtist
 }
-
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -280,8 +299,16 @@ Use addArtist to do the following:
 4. Return the array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(targetArray, name, years, genre, nationality, bio) {
+  const newArtist = {
+    "name": name,
+    "years": years,
+    "genre": genre,
+    "nationality": nationality,
+    "bio": bio 
+  }
+  targetArray.push(newArtist)
+  return targetArray
 }
 
 
@@ -294,8 +321,14 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(targetArray) {
+  const prolific = []
+  for (let i = 0; i < targetArray.length; i++) {
+    if (targetArray[i]["paintings"] > 100) {
+      prolific.push(targetArray[i]["name"])
+    }
+  }
+  return prolific
 }
 
 
@@ -310,8 +343,12 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(targetArray,targetArtist){
+    for (let i = 0; i < targetArray.length; i++) {
+      if (targetArray[i]["name"] === targetArtist) {
+        return targetArray[i]["bio"]
+      }
+    }
 }
 
 
@@ -325,8 +362,14 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(targetArray,targetNationality) {
+  const artistsOfNationality = []
+  for (let i = 0; i < targetArray.length; i++) {
+    if (targetArray[i]["nationality"] === targetNationality) {
+      artistsOfNationality.push(targetArray[i]["name"])
+    }
+  }
+  return artistsOfNationality
 }
 
 
